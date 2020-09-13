7143991f20074272060abd9c9effa6987ddb4ea2
MConcoba <mconcoba20@gmail.com>
2020-09-13 09:17:27 -0600

    Create auth services of the funcion api

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,112 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError, map } from 'rxjs/operators';
+import {
+  Roles,
+  User,
+  UserInterface,
+  UserResponse,
+} from '../models/user-interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthServiceService {
+export class AuthService {
+  public loggedIn = new BehaviorSubject<boolean>(false);
+  public role = new BehaviorSubject<Roles>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
+  headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  registerUser(userDataRegister: User): Observable<any> {
+    const url_api = 'http://localhost:3000/api/Users';
+    return this.http
+      .post<UserInterface>(url_api, userDataRegister, { headers: this.headers })
+      .pipe(map((data) => data));
+  }
+
+  loginuser(authData: User): Observable<any> {
+    const url_api = 'http://localhost:3000/api/login';
+    return this.http
+      .post<UserInterface>(url_api, authData)
+      .pipe(map((data) => data));
+  }
+
+  setUser(user: UserInterface): void {
+    let user_string = JSON.stringify(user);
+    localStorage.setItem('currentUser', user_string);
+  }
+
+  setToken(token): void {
+    localStorage.setItem('accessToken', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('accessToken');
+  }
+
+  getCurrentUser(): UserInterface {
+    let user_string = localStorage.getItem('currentUser');
+    if (user_string != null) {
+      let user: UserInterface = JSON.parse(user_string);
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  logoutUser() {
+    let accessToken = localStorage.getItem('accessToken');
+    const url_api = `http://localhost:3000/api/Users/logout?access_token=${accessToken}`;
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('currentUser');
+    return this.http.post<UserInterface>(url_api, { headers: this.headers });
+  }
+  get isLogged(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  get isAdmin$(): Observable<string> {
+    return this.role.asObservable();
+  }
+
+  // ------------------------------------------------------------------------------------------------------------------
+
+  login(authData: User): Observable<UserResponse | void> {
+    return this.http
+      .post<UserResponse>(`http://localhost:3000/api/login`, authData)
+      .pipe(
+        map((res: UserResponse) => {
+          this.saveLocalStorage(res);
+          this.loggedIn.next(true);
+          this.role.next(res.role);
+        }),
+        catchError((err) => this.handlerError(err))
+      );
+  }
+
+  private saveLocalStorage(user: UserResponse): void {
+    const { id, message, ...rest } = user;
+    localStorage.setItem('user', JSON.stringify(rest));
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+    this.loggedIn.next(false);
+    this.router.navigate(['/user/login']);
+  }
+
+  private handlerError(err): Observable<never> {
+    let errorMessage = 'An error occured retrienving data';
+    if (err) {
+      errorMessage = `Error: code ${err.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 }
