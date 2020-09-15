bce46a057adb674d7833e91bb8b22ff6fcf1e075
MConcoba <mconcoba20@gmail.com>
2020-09-15 16:54:13 -0600

    Funcions Magazines all

---
+++ api/src/models/magazine.model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MagazineSchema = Schema({
+	author: String,
+	title: String,
+	edicion: String,
+	description: String,
+	frequencyActs: String,
+	specimens: Number,
+	topics: [String],
+	keywords: [String],
+	copies: Number,
+	available: Number,
+});
+
+module.exports = mongoose.model('magazine', MagazineSchema);
---
+++ api/src/routers/magazine.route.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express');
+var MagazineController = require('../controllers/magazine.controller');
+var md_auth = require('../middleware/autentificated');
+
+var api = require('express-promise-router')();
+
+api.post('/new-magazine', md_auth.ensureAuth, MagazineController.newMagazine);
+api.get('/get-magazines', MagazineController.getMagazines);
+api.get('/get-magazine/:idMagazine', MagazineController.getMagazine);
+api.put('/update-magazine/:idMagazine', md_auth.ensureAuth, MagazineController.updateMagazine);
+api.delete('/delete-magazine/:idMagazine', md_auth.ensureAuth, MagazineController.deleteMagazine);
+api.get('/search-magazine-title', MagazineController.searchTitle);
+api.get('/search-magazine-keywords', MagazineController.searchKeywords);
+api.get('/lend-magazine/:idMagazine', md_auth.ensureAuth, MagazineController.lendMagazine);
+api.get('/return-magazine/:idMagazine', md_auth.ensureAuth, MagazineController.returnMagazine);
+
+module.exports = api;
