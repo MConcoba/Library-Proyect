a63077054bb79a77f27e39f6731acfcbdec63001
MConcoba <mconcoba20@gmail.com>
2020-09-15 18:01:25 -0600

    Crear view profile user

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { UserResponse } from 'src/app/models/user-interface';
+import { AuthService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+  constructor(private authSvc: AuthService) {}
+  filterPost = '';
+  isLogged = false;
+  isAdmin = null;
 
-  constructor() { }
+  private destroy$ = new Subject<any>();
+  public users: UserResponse;
 
   ngOnInit(): void {
+    this.getListUsers();
+    this.authSvc.isLogged
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => (this.isLogged = res));
+
+    this.authSvc.isAdmin$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => (this.isAdmin = res));
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next({});
+    this.destroy$.complete();
+  }
+
+  getListUsers(): any {
+    this.authSvc
+      .getUserProfile()
+      .subscribe((user: UserResponse) => (this.users = user));
+  }
 }
